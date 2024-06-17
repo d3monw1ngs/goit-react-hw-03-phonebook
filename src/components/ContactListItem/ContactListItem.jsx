01f2b71cd27d33a24328bc7cecf2658a5832e19d
d3monw1ngs <tezzielumsit87@gmail.com>
2024-06-17 23:28:47 +0800

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,9 +1,27 @@
-import PropTypes from 'prop-types';
+import { Component } from 'react';
 import css from './ContactListItem.module.css';
+import PropTypes from 'prop-types';
 
-export const ContactListItem = ({ contact, onDelete }) => {
-  return (
-    <li className={css.contactItem}>
+
+export class ContactListItem extends Component {
+  static propTypes = {
+    contact: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired,
+    onDelete: PropTypes.func.isRequired,
+  };
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount()');
+  }
+
+  render() {
+    const { contact, onDelete } = this.props;
+
+    return (
+      <li className={css.contactItem}>
       <div className={css.contactDetails}>
         <span className={css.contactName}>{contact.name}</span>:{" "}
         <span className={css.contactNumber}>{contact.number}</span>     
@@ -12,14 +30,8 @@ export const ContactListItem = ({ contact, onDelete }) => {
       Delete
     </button>
   </li>
-  );
-};
+    );
+  }
+}
+    
 
-ContactListItem.propTypes = {
-    contact: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-        }).isRequired,
-    onDelete: PropTypes.func.isRequired,
-};
